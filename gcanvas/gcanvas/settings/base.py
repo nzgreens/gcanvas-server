ed537016fc0f6614db752c019e07f249f66541ce
James Hurford <terrasea@gmail.com>
2014-05-07 09:22:29 +1200

    Setup the initial skeleton project

---
+++ gcanvas/gcanvas/settings/base.py
@@ -0,0 +1,77 @@
+"""
+Django settings for gcanvas project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+from unipath import Path
+
+BASE_DIR = Path(__file__).ancestor(3)
+MEDIA_ROOT = BASE_DIR.child('media')
+STATIC_ROOT = BASE_DIR.child('static')
+STATICFILES_DIRS = (
+    BASE_DIR.child('assets'),
+)
+
+TEMPLATE_DIRS = (
+    BASE_DIR.child('templates'),
+)
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'yzao_xm(qu7&bce6cp=$x9hh3ky*3ta%-h)q%l(a@lyqys4(s*'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'gcanvas.urls'
+
+WSGI_APPLICATION = 'gcanvas.wsgi.application'
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
---
+++ gcanvas/gcanvas/settings/local.py
@@ -0,0 +1,20 @@
+#settings/local.py
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "gcanvas",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
+    }    
+}
+
+INSTALLED_APPS += {"debug_toolbar", }
